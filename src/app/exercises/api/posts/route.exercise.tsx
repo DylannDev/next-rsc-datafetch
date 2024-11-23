621c34e1083bd3969cf4b82cd80d0e6518470122
dylann97 <d.xavero@hotmail.com>
2024-11-24 00:26:21 +0100

    exercise done

--- src/app/exercises/api/posts/route.exercise.tsx
+++ src/app/exercises/api/posts/route.exercise.tsx
@@ -1,13 +1,7 @@
 // 🐶 Importe la fonction `getPosts` qui va récupérer les posts en BDD
-//import {getPosts} from '@/db/sgbd'
+import {getPosts} from '@/db/sgbd'
 
 export async function GET() {
-  // 🐶 Appelle la fonction `getPosts` pour récupérer les posts en BDD
-
-  // 🐶 Retourne les données en json dans la réponse http
-  // ⛏️ Supprime `[]` et remplace le par la `data`
-  return Response.json([])
-  // 🐶 Constate la présence des données ici:  http://localhost:3000/exercises/api/posts
-
-  // 🐶 N'oublie pas les exercices bonus
+  const posts = await getPosts()
+  return Response.json(posts)
 }
