8a2487597b823c0e72bd506b057a355e61f0f1ff
dylann97 <d.xavero@hotmail.com>
2024-11-26 12:20:33 +0100

    exercise done

--- src/app/exercises/post/page.exercise.tsx
+++ src/app/exercises/post/page.exercise.tsx
@@ -1,25 +1,14 @@
-// 'use client'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+'use client'
 import {Post} from '@/lib/type'
-// 🐶 Importe `useEffect` et `useState` car ils seront utiles pour récupérer les datas
 import {useEffect, useState} from 'react'
 
-// 🐶 Une erreur devrait apparaître, corrige la car nous sommes dans un composant client
-
 const Page = () => {
-  // 🐶 Crée un state pour les posts
   const [posts, setPosts] = useState<Post[]>([])
-
-  // 🐶 Crée un effet pour récupérer les posts
-  // 🤖 Utilise le code ci-dessous
-
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/posts'
-        )
+        // ⛏️ Remplace cette url par l'url de l'api next
+        const response = await fetch('http://localhost:4000/posts')
         const data = await response.json()
         setPosts(data as Post[])
       } catch (error) {
@@ -32,7 +21,6 @@ const Page = () => {
     <div className="mx-auto max-w-4xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold"> Fetch Posts</h1>
       <ul className="list-disc p-4 pl-4">
-        {/* 🐶 Affiche les posts */}
         {posts?.map((post: Post) => <li key={post.title}>{post.title}</li>)}
       </ul>
     </div>
